4df3326384bd1e786d6b9e75bc94f1753dc48621
simonsajjizoft <simon.sajimathews@zoftsolutions.com>
2024-07-11 10:12:25 +0530

    Prosync Integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:7071/api/',
+   // apiUrl: 'http://localhost:7071/api/',
+   apiUrl: 'https://func-scf-presenter-dev.azurewebsites.net/api/',
   authApiUrl: '',
   flowManageUrl: '',
   coreApiUrl:''
